a9aa040928f8b3fdf982270cc7b5bd4634b8486d
rishabr17 <rishabravikumar@yahoo.com>
2018-09-12 11:17:21 -0400

    site

--- core/server.js
+++ core/server.js
@@ -1,26 +1,31 @@
 ﻿var http = require("http");
 var emp = require("../controllers/chats");
 var settings = require("../settings");
+var fs = require('fs');
 
-http.createServer(function (req, resp) {
-    switch (req.method) {
-        case "GET":
-            if (req.url === "/") {
-                resp.end();
-            }
-            else if (req.url === "/chats") {
-                emp.getList(req, resp);
-            }
-            break;
-        case "POST":
-            break;
-        case "PUT":
-            break;
-        case "DELETE":
-            break;
-        default:
-            break;
-    }
-}).listen(process.env.PORT || 5000, function() {
-    console.log("Started listening at: " + process.env.PORT);
+fs.readFile('index.html', function (err, html) {
+    if (err) throw err;
+
+    http.createServer(function (req, resp) {
+        switch (req.method) {
+            case "GET":
+                if (req.url === "/") {
+                    resp.end();
+                }
+                else if (req.url === "/chats") {
+                    emp.getList(req, resp);
+                }
+                break;
+            case "POST":
+                break;
+            case "PUT":
+                break;
+            case "DELETE":
+                break;
+            default:
+                break;
+        }
+    }).listen(process.env.PORT || 5000, function () {
+        console.log("Started listening at: " + process.env.PORT);
+    })
 });
