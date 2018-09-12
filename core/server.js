54f0ccb0552b6170a746c6a8f99023ee32787cca
rishabr17 <rishabravikumar@yahoo.com>
2018-09-12 11:45:18 -0400

    site

--- core/server.js
+++ core/server.js
@@ -3,10 +3,17 @@ var emp = require("../controllers/chats");
 var settings = require("../settings");
 var fs = require('fs');
 
+
 fs.readFile('index.html', function (err, html) {
     if (err) throw err;
 
-    http.createServer(function (req, resp) {
+http.createServer(function (req, resp) {
+
+    if (req.url === "/") {
+        resp.writeHeader(200, { "Content-Type": "text/html" });
+        resp.write(html);
+    }
+    
         switch (req.method) {
             case "GET":
                 if (req.url === "/") {
